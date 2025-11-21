b4cdf24e69b66a9527d8b40e5f0ca1bd0998103a
tamil-2005 <tamilff2005@gmail.com>
2025-11-21 20:49:17 +0530

    neww

--- api/events.js
+++ api/events.js
@@ -1,12 +1,19 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Cache-Control", "no-cache, no-transform");
   res.setHeader("Connection", "keep-alive");
+  res.flushHeaders(); // ⭐ important on Vercel to start SSE immediately
 
   if (!global.clients) global.clients = [];
   global.clients.push(res);
 
+  // heartbeat to keep connection alive on Vercel
+  const keepAlive = setInterval(() => {
+    res.write(":\n\n"); // SSE comment event
+  }, 15000);
+
   req.on("close", () => {
+    clearInterval(keepAlive);
     global.clients = global.clients.filter(c => c !== res);
   });
 }
